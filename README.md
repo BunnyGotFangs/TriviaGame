9b7a07c7b282b76b411e6c943e26349948e734c9
BunnygotFangs <kimberly@thehilts.com>
2018-04-13 21:14:59 -0500

    add instruction and start button, update read me

--- README.md
+++ README.md
@@ -14,17 +14,28 @@ WHY IT IS USEFUL:
 
 START FROM HERE: 
 
+    1. This is a timed game.
+    2. There are 12 questions in total.
+    3. Questions get progressively harder as the game proceeds.
+    4. You have 10 seconds to answer each question.
+    5. When the 10 seconds are up, the game will move to the next question.
+    6. Read the question and select the correct answer.
+    7. Game ends either when time runs out, or you have answered all 12 questions.
+    8. Click the Green GIDDY UP button to get started.
 
-https://bunnygotfangs.github.io/TriviaGame/
+To get to the game copy the URL below and paste into your browser.
+
+    https://bunnygotfangs.github.io/TriviaGame/
+
 
 NEED HELP or HAVE SUGGESTIONS:
 
-Have questions or need help with the code, the game mechanics or have suggestions then please feel free to contact me at my website:
+    Have questions or need help with the code, the game mechanics or have suggestions then please feel free to contact me at my website:
 
-https://bunnygotfangs.github.io/Bootstrap-Portfolio/contact.html
+    https://bunnygotfangs.github.io/Bootstrap-Portfolio/contact.html
 
 CONTRIBUTIONS:
 
-many thanks to all the trailblazers before me who actually got their attack to work right! To those who survived coding BootCamp! -- I truly hope you have landed where you want to be as this is a journey, a sprint, a marathon, and an F&*$'in HILL CLIMB! 
+    Many thanks to all the trailblazers before me who actually got their attack to work right! To those who survived coding BootCamp! -- I truly hope you have landed where you want to be as this is a journey, a sprint, a marathon, and an F&*$'in HILL CLIMB! 
 
-Thanks for having a sense of humor!
+    Thanks for having a sense of humor!
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,11 +10,43 @@ $(document).ready(function()
 // counter to answer questions == 10 seconds like on HQ   
     var timer = 10;
 //list of questions (12 from easy to hard)-- trying a multitier array object
-    var questions = ["Which animal has 4 legs?",2,3,4,5,6,7,8,9,10,11,12];
+    var questions = [   "Which animal has 4 legs?",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        ""];
 //answers to the questions again trying the multi-tier array object    
-    var answers = [["Fish", "Bird", "Horse", "Fly"], [], ];
+    var answers = [ ["Fish", "Bird", "Horse", "Fly"], 
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    [], ];
  // list of the right answers  
-    var rightResponse = ["Horse"];
+    var rightResponse = [   "Horse",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",   ];
 // countdown on each question
     var qaCounter = 0;
 // how to capture answer clicks    
@@ -30,13 +62,29 @@ $(document).ready(function()
 // if you can get sounds to play    
     var click = new Audio("");
 
-// Provide instructions for game play
+// Provide instructions for game play, add to READ ME
 
-// Game functions here
+alert("This is a timed game.");
+alert("There are 12 questions in total.");
+alert("Questions get progressively harder as the game proceeds.");
+alert("You have 10 seconds to answer each question.");
+alert("When the 10 seconds are up the game will move to the next question.");
+alert("Read the question and select the correct answer.");
+alert("Game ends either when time runs out or you have answered all 12 questions.");
+alert("Click the Green LET'S GIDDY UP button to get started.");
 
-// per Tutor functions to go here, snaller the better
 
+$(document).ready(function() 
 
-
+{
+    // Create a function that adds the start button to the game
+    //Cannot figure out the centering
+    function addStartButton() 
+    {
+        startup = "<button type=\"button\" class=\"btn btn-success\">Let's Giddy Up</button>";
+        $(".mainArea").html(startup);
+    }
     
+    addStartButton();
+})
 })
